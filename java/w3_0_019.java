72b580ff3676dc7f48cdf20c21351a18976a8b7c
Erik Heen <heenx008@umn.edu>
2018-12-17 18:42:43 -0600

    0_019, 0_020, 0_xxx

---
+++ java/w3_0_019.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class w3_0_019{
+    public static void main(String[] args){
+        int numIn;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Input a decimal: ");
+        numIn = scan.nextInt();
+        System.out.print("Binary number is: "+Integer.toBinaryString(numIn));
+        System.out.println("\nEND\n");
+    }
+}
