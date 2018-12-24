0f0b3ec1a322e1b529e1848ab241ba57333a35f3
Erik Heen <erikheen320@gmail.com>
2018-12-24 02:08:14 -0600

    testing more C

---
+++ C/sizeofDemo.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main() {
+   printf("Storage size for int  : %d \n", sizeof(int));
+   printf("Storage size for long : %d \n", sizeof(long));
+
+   return 0;
+}
